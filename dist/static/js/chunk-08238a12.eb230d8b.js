9ce7394df632a219c152400c2f862b075ff0f8d4
ygbin <yuguobin@yuansfer.com>
2020-11-05 10:23:47 +0800

    ...

---
+++ dist/static/js/chunk-08238a12.eb230d8b.js
@@ -0,0 +1,500 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["chunk-08238a12"],{
+
+/***/ "08e0":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/y_ill-09-1.1082bb00.png";
+
+/***/ }),
+
+/***/ "11af":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/rectangle-34.5bddc7a1.png";
+
+/***/ }),
+
+/***/ "13cd":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-004.a910f582.png";
+
+/***/ }),
+
+/***/ "28b1":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"180b065a-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/company/index.vue?vue&type=template&id=324d5a5e&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"container"},[_c('div',{staticClass:"content-wrap wrap-one"},[_c('div',{staticClass:"content-left"},[_vm._m(0),_c('div',{staticClass:"content-bd"},[_vm._v(" We started Yuansfer with a mission to drive the businesses' growth by connecting them with consumers globally through better commerce infrastructures ")]),_c('div',{staticClass:"content-ft"},[_c('el-button',{staticClass:"grad-btn"},[_vm._v("GET DEMO"),_c('i',{staticClass:"el-icon-right el-icon--right"})])],1)]),_c('div',{staticClass:"content-right"},[_c('div',{staticClass:"img-box"},[_c('el-image',{attrs:{"src":__webpack_require__("c05f")}})],1)])]),_c('div',{staticClass:"content-wrap wrap-two"},[_c('div',{staticClass:"content-hd"},[_c('el-image',{attrs:{"src":__webpack_require__("08e0")}})],1),_vm._m(1)]),_c('div',{staticClass:"content-wrap wrap-three"},[_vm._m(2),_c('div',{staticClass:"content-bd"},[_c('ul',{staticClass:"team-list"},[_vm._l((_vm.teamList),function(item){return [_c('li',{key:item.img,staticClass:"team-item"},[_c('div',{staticClass:"item-box"},[_c('div',{staticClass:"img"},[_c('el-image',{staticClass:"avatar-img",attrs:{"src":__webpack_require__("dd28")("./" + item.img)}})],1),_c('div',{staticClass:"label"},[_vm._v(_vm._s(item.name))]),_c('div',{staticClass:"desc"},[_vm._v(_vm._s(item.job))]),_c('div',{staticClass:"link"},[_c('a',{staticClass:"link-a",attrs:{"href":item.link}},[_c('span',{staticClass:"text"},[_vm._v("Linkedin")]),_c('el-image',{staticClass:"arrow-icon",attrs:{"src":__webpack_require__("5dc3")}})],1)])])])]})],2)])]),_c('div',{staticClass:"content-wrap wrap-four"},[_c('div',{staticClass:"content-hd"},[_vm._v("Investors")]),_c('div',{staticClass:"content-bd"},[_c('div',{staticClass:"left"},[_vm._v(" We're lucky to be supported by an impressive group of advisors and investors who also believe in our mission of building an improved global commerce infrastructure. ")]),_c('div',{staticClass:"right"},[_c('div',{staticClass:"item"},[_c('el-image',{attrs:{"src":__webpack_require__("79ef")}})],1),_c('div',{staticClass:"item"},[_c('el-image',{attrs:{"src":__webpack_require__("6442")}})],1)])])]),_c('div',{staticClass:"content-wrap wrap-five"},[_c('div',{staticClass:"content-hd"},[_c('div',{staticClass:"img-box"},[_c('el-image',{attrs:{"src":__webpack_require__("bafc")}})],1),_vm._m(3)]),_c('div',{staticClass:"content-bd"},[_c('el-button',{staticClass:"grad-btn"},[_vm._v("explore open roles")])],1)]),_c('div',{staticClass:"content-wrap wrap-six"},[_c('div',{staticClass:"content-bd"},[_vm._m(4),_c('div',{staticClass:"desc"},[_vm._v("Do not hesitate to contact us")]),_c('div',{staticClass:"btn-box"},[_c('el-button',{staticClass:"grad-btn"},[_vm._v("CONTACT US")])],1)])])])}
+var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"content-hd"},[_vm._v(" Commerce "),_c('span',{staticClass:"blue"},[_vm._v("Without")]),_vm._v(" Borders ")])},function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"content-bd"},[_c('div',{staticClass:"label"},[_vm._v(" About "),_c('span',{staticClass:"blue"},[_vm._v("Yuansfer")])]),_c('div',{staticClass:"desc"},[_vm._v(" Founded in 2017, Yuansfer is a leading alternative payment platform that helps businesses reach consumers around the world. Thousands of American businesses are already using Yuansfer to accept and make payments. ")])])},function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"content-hd"},[_vm._v(" Our "),_c('span',{staticClass:"blue"},[_vm._v("Team")])])},function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"content-box"},[_c('div',{staticClass:"label"},[_vm._v("Join the Team")]),_c('div',{staticClass:"desc"},[_vm._v("Help businesses grow beyond their borders.")])])},function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"label"},[_vm._v("Have a question about "),_c('span',{staticClass:"blue"},[_vm._v("Yuansfer's payment")]),_vm._v(" platform?")])}]
+
+
+// CONCATENATED MODULE: ./src/views/company/index.vue?vue&type=template&id=324d5a5e&scoped=true&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/company/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var companyvue_type_script_lang_js_ = ({
+  name: '',
+  data: function data() {
+    return {
+      teamList: [{
+        img: 'team-001.png',
+        name: 'Mason Lin',
+        job: 'Founder and CEO',
+        link: ''
+      }, {
+        img: 'team-002.png',
+        name: 'Jerome d’Orchimont',
+        job: 'CRO',
+        link: ''
+      }, {
+        img: 'team-003.png',
+        name: 'Kai Zhou',
+        job: 'CTO',
+        link: ''
+      }, {
+        img: 'team-004.png',
+        name: 'Amy Liu',
+        job: 'Business Developer',
+        link: ''
+      }, {
+        img: 'team-005.png',
+        name: 'Ashley Wang',
+        job: 'Operations Support',
+        link: ''
+      }, {
+        img: 'team-006.png',
+        name: 'Xi Lin',
+        job: 'Operations Manager',
+        link: ''
+      }, {
+        img: 'team-007.png',
+        name: 'Frank Liu',
+        job: 'Head of Integration',
+        link: ''
+      }, {
+        img: 'team-008.png',
+        name: 'Luca Veneziano',
+        job: 'Solution Engineer',
+        link: ''
+      }, {
+        img: 'team-009.png',
+        name: 'Fly Zhu',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-010.png',
+        name: 'Zewen Liu',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-011.png',
+        name: 'Guobin Yu',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-012.png',
+        name: 'Jiaxing Dong',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-013.png',
+        name: 'Graziano de Boni',
+        job: 'Advisor',
+        link: ''
+      }, {
+        img: 'team-014.png',
+        name: 'Tom Demeri',
+        job: 'Advisor',
+        link: ''
+      }, {
+        img: 'team-015.png',
+        name: 'Joe Dela Cruz',
+        job: 'Advisor',
+        link: ''
+      }, {
+        img: 'team-016.png',
+        name: 'Doug Weich',
+        job: 'Advisor',
+        link: ''
+      }]
+    };
+  },
+  created: function created() {},
+  methods: {}
+});
+// CONCATENATED MODULE: ./src/views/company/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var views_companyvue_type_script_lang_js_ = (companyvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./src/views/company/index.vue?vue&type=style&index=0&id=324d5a5e&rel=stylesheet%2Fscss&lang=scss&scoped=true&
+var companyvue_type_style_index_0_id_324d5a5e_rel_stylesheet_2Fscss_lang_scss_scoped_true_ = __webpack_require__("de63");
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/views/company/index.vue
+
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  views_companyvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  "324d5a5e",
+  null
+  
+)
+
+/* harmony default export */ var company = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "3220":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-008.b7f96914.png";
+
+/***/ }),
+
+/***/ "3cea":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-001.329d6333.png";
+
+/***/ }),
+
+/***/ "413b":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-bg.92a5a190.png";
+
+/***/ }),
+
+/***/ "4db3":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-006.d5b29fac.png";
+
+/***/ }),
+
+/***/ "5780":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-007.df6c2b2f.png";
+
+/***/ }),
+
+/***/ "58d5":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/group-69.a1c5409e.png";
+
+/***/ }),
+
+/***/ "5dc3":
+/***/ (function(module, exports) {
+
+module.exports = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAJCAYAAADHP4f4AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACHSURBVHgBtZLLCYVADEVvZtw/S5hSfCDoUjvQkqxgwAbcurMVS7ABJ0b8ILrNHAjkZnMgCUGR3HNmLTyt+I8tzdfcQB/HFlPp2cWUfERU9OyhRypVPfISZHWJNA3ikRqDIQGjA+EHHZxUdifGHAJqgiLnd02XgMLxZXEO/xDsUVUia1negp0NEiouZ4dDB+QAAAAASUVORK5CYII="
+
+/***/ }),
+
+/***/ "6442":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/image-28.d3caec54.png";
+
+/***/ }),
+
+/***/ "6b8e":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-011.27a9a44a.png";
+
+/***/ }),
+
+/***/ "72d6":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/group-70.d99182e5.png";
+
+/***/ }),
+
+/***/ "79ef":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/image-27.c8cec89b.png";
+
+/***/ }),
+
+/***/ "7dc8":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-010.fe8a7b05.png";
+
+/***/ }),
+
+/***/ "7e9f":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-014.f4d3e644.png";
+
+/***/ }),
+
+/***/ "805c":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-003.b0fe97f4.png";
+
+/***/ }),
+
+/***/ "84a9":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-009.2ca1dfdb.png";
+
+/***/ }),
+
+/***/ "8809":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-013.d6854121.png";
+
+/***/ }),
+
+/***/ "88ce":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "8adb":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-015.f7b676c3.png";
+
+/***/ }),
+
+/***/ "bafc":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/group-pack.bc6f8b9d.png";
+
+/***/ }),
+
+/***/ "bb92":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-016.ef6c63c2.png";
+
+/***/ }),
+
+/***/ "c05f":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/y_ill-06-2.38e451da.png";
+
+/***/ }),
+
+/***/ "c071":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-012.01f9c9c1.png";
+
+/***/ }),
+
+/***/ "c14b":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-002.9232b255.png";
+
+/***/ }),
+
+/***/ "caaf":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/team-005.1468d9c3.png";
+
+/***/ }),
+
+/***/ "dd28":
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./arrow-right.png": "5dc3",
+	"./group-69.png": "58d5",
+	"./group-70.png": "72d6",
+	"./group-pack.png": "bafc",
+	"./image-27.png": "79ef",
+	"./image-28.png": "6442",
+	"./rectangle-34.png": "11af",
+	"./team-001.png": "3cea",
+	"./team-002.png": "c14b",
+	"./team-003.png": "805c",
+	"./team-004.png": "13cd",
+	"./team-005.png": "caaf",
+	"./team-006.png": "4db3",
+	"./team-007.png": "5780",
+	"./team-008.png": "3220",
+	"./team-009.png": "84a9",
+	"./team-010.png": "7dc8",
+	"./team-011.png": "6b8e",
+	"./team-012.png": "c071",
+	"./team-013.png": "8809",
+	"./team-014.png": "7e9f",
+	"./team-015.png": "8adb",
+	"./team-016.png": "bb92",
+	"./team-bg.png": "413b",
+	"./y_ill-06-2.png": "c05f",
+	"./y_ill-09-1.png": "08e0"
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	if(!__webpack_require__.o(map, req)) {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return map[req];
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = "dd28";
+
+/***/ }),
+
+/***/ "de63":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_324d5a5e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("88ce");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_324d5a5e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_324d5a5e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_324d5a5e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ })
+
+}]);
---
+++ dist/static/js/chunk-7e5c95c8.41d539e2.js
@@ -0,0 +1,272 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["chunk-7e5c95c8"],{
+
+/***/ "6c16":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "cddd":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"180b065a-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/payment/index.vue?vue&type=template&id=e6d0878e&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"container"},[_c('div',{staticClass:"content-wrap wrap-one"},[_c('div',{staticClass:"content-left"},[_vm._m(0),_c('div',{staticClass:"content-bd"},[_vm._v(" Use Yuansfer to compensate and manage your influencers, sellers, and contractors across 180+ countries, including China and other emerging markets. ")]),_c('div',{staticClass:"content-ft"},[_c('el-button',{staticClass:"grad-btn"},[_vm._v("GET DEMO"),_c('i',{staticClass:"el-icon-right el-icon--right"})])],1)]),_c('div',{staticClass:"content-right"},[_c('div',{staticClass:"img-box"},[_c('el-image',{attrs:{"src":__webpack_require__("e8e4")}})],1)])]),_vm._m(1),_c('div',{staticClass:"content-wrap wrap-three"},[_vm._m(2),_c('div',{staticClass:"content-bd"},[_c('ul',{staticClass:"team-list"},[_vm._l((_vm.teamList),function(item){return [_c('li',{key:item.img,staticClass:"team-item"},[_c('div',{staticClass:"item-box"},[_c('div',{staticClass:"img"}),_c('div',{staticClass:"label"},[_vm._v(_vm._s(item.name))]),_c('div',{staticClass:"desc"},[_vm._v(_vm._s(item.job))]),_c('div',{staticClass:"link"},[_c('a',{staticClass:"link-a",attrs:{"href":item.link}},[_c('span',{staticClass:"text"},[_vm._v("Linkedin")])])])])])]})],2)])])])}
+var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"content-hd"},[_vm._v(" Simplify Domestic and "),_c('span',{staticClass:"blue"},[_vm._v("International")]),_vm._v(" Payouts ")])},function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"content-wrap wrap-two"},[_c('div',{staticClass:"content-hd"}),_c('div',{staticClass:"content-bd"},[_c('div',{staticClass:"label"},[_vm._v(" About "),_c('span',{staticClass:"blue"},[_vm._v("Yuansfer")])]),_c('div',{staticClass:"desc"},[_vm._v(" Founded in 2017, Yuansfer is a leading alternative payment platform that helps businesses reach consumers around the world. Thousands of American businesses are already using Yuansfer to accept and make payments. ")])])])},function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"content-hd"},[_vm._v(" Our "),_c('span',{staticClass:"blue"},[_vm._v("Team")])])}]
+
+
+// CONCATENATED MODULE: ./src/views/payment/index.vue?vue&type=template&id=e6d0878e&scoped=true&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/views/payment/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var paymentvue_type_script_lang_js_ = ({
+  name: '',
+  data: function data() {
+    return {
+      teamList: [{
+        img: 'team-001.png',
+        name: 'Mason Lin',
+        job: 'Founder and CEO',
+        link: ''
+      }, {
+        img: 'team-002.png',
+        name: 'Jerome d’Orchimont',
+        job: 'CRO',
+        link: ''
+      }, {
+        img: 'team-003.png',
+        name: 'Kai Zhou',
+        job: 'CTO',
+        link: ''
+      }, {
+        img: 'team-004.png',
+        name: 'Amy Liu',
+        job: 'Business Developer',
+        link: ''
+      }, {
+        img: 'team-005.png',
+        name: 'Ashley Wang',
+        job: 'Operations Support',
+        link: ''
+      }, {
+        img: 'team-006.png',
+        name: 'Xi Lin',
+        job: 'Operations Manager',
+        link: ''
+      }, {
+        img: 'team-007.png',
+        name: 'Frank Liu',
+        job: 'Head of Integration',
+        link: ''
+      }, {
+        img: 'team-008.png',
+        name: 'Luca Veneziano',
+        job: 'Solution Engineer',
+        link: ''
+      }, {
+        img: 'team-009.png',
+        name: 'Fly Zhu',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-010.png',
+        name: 'Zewen Liu',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-011.png',
+        name: 'Guobin Yu',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-012.png',
+        name: 'Jiaxing Dong',
+        job: 'Engineer',
+        link: ''
+      }, {
+        img: 'team-013.png',
+        name: 'Graziano de Boni',
+        job: 'Advisor',
+        link: ''
+      }, {
+        img: 'team-014.png',
+        name: 'Tom Demeri',
+        job: 'Advisor',
+        link: ''
+      }, {
+        img: 'team-015.png',
+        name: 'Joe Dela Cruz',
+        job: 'Advisor',
+        link: ''
+      }, {
+        img: 'team-016.png',
+        name: 'Doug Weich',
+        job: 'Advisor',
+        link: ''
+      }]
+    };
+  },
+  created: function created() {},
+  methods: {}
+});
+// CONCATENATED MODULE: ./src/views/payment/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var views_paymentvue_type_script_lang_js_ = (paymentvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./src/views/payment/index.vue?vue&type=style&index=0&id=e6d0878e&rel=stylesheet%2Fscss&lang=scss&scoped=true&
+var paymentvue_type_style_index_0_id_e6d0878e_rel_stylesheet_2Fscss_lang_scss_scoped_true_ = __webpack_require__("e6f4");
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/views/payment/index.vue
+
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  views_paymentvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  "e6d0878e",
+  null
+  
+)
+
+/* harmony default export */ var payment = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "e6f4":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_e6d0878e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("6c16");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_e6d0878e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_e6d0878e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_e6d0878e_rel_stylesheet_2Fscss_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
+/***/ "e8e4":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/img/y_ill-08-1.9f46cf72.png";
+
+/***/ })
+
+}]);
